db9b7220e061009d36d04eda27a22e6890694160
halex02 <halex02@msn.com>
2016-03-01 15:41:19 +0100

    maj

--- TPs/TP03/SendUdp.c
+++ TPs/TP03/SendUdp.c
@@ -4,33 +4,40 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
 
-int sender(){
-  int socket_server;
-  socket_server=socket(AF_INET, SOCK_STREAM, 0);
-  if(socket_server==-1){
-    perror("socket_server");
-    return -1;
-    }
+void sender(const char * message){
+
+    int sock;
+    struct sockaddr_in saddr;
+    
+    saddr.sin_family=AF_INET;
+    saddr.sin_port=htons(5001);
+    saddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
  
+    sock=socket(saddr.sin_family, SOCK_DGRAM, 0);
+    if(sock<0){
+        perror("sock");
+        exit(EXIT_FAILURE);
+    }
+    if(sendto(sock, message,strlen(message)+1, 0,(struct sockaddr *)&saddr, sizeof(saddr) ) < 0){
+        perror("erreur a l'envoie") ;
+        exit(EXIT_FAILURE) ;
+    }
+    
+    close(sock);
   
-  return socket_server;
+    
 }
 
 int main (int argc, char* argv[]) {
   
-  if(argc < 2)
-    exit(EXIT_FAILURE) ;
-
-  int socket = sender() ;
- struct sockaddr_in saddr;
-  saddr.sin_family=AF_INET;
-  saddr.sin_port=5001;
-  saddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-  if(sendto(socket, argv[1],strlen(argv[1])+1, 0,(struct sockaddr *)&saddr, sizeof(saddr) ) == (-1)){
-    perror("erreur a l'envoie") ;
-    exit(EXIT_FAILURE) ;
-  }
+    if (argc < 2){
+        return EXIT_FAILURE;
+    }  
+    
+    sender(argv[1]);
   
-  return EXIT_SUCCESS ;
+    return EXIT_SUCCESS ;
 }
